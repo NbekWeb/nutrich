560af5369581536c4414bfa4fa9c1c2b77522bf1
NbekWeb <nbekdev@gmail.com>
2025-07-02 23:22:26 +0500

    s

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/home',
+        destination: '/user',
         permanent: true,
         locale: false
       }
